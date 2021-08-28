e186aef7a816bc51cf1b7181df31eb7cc06335dc
NovoEstilo <89430984+NovoEstilo@users.noreply.github.com>
2021-08-27 22:07:58 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function abrelist(){
+  var abreLista=document.querySelector(".list");
+  abreLista.style.height='72vw';
+  abreLista.style.display='block';
+  abreLista.style.transition='.3s';
+}
+function fechalist(){
+  var fechaLista=document.querySelector(".list");
+  fechaLista.style.display='none';
+}
